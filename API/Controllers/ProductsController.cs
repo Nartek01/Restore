710dad5664d7810b8ef8de63ce319def8422bec1
Master Nara Tekchuen <nartek01@gmail.com>
2023-01-10 19:53:59 +0100

    Added get all products endpoint

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,6 +10,17 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly StoreContext context;
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var products = context.Products.ToList();
 
+            return Ok(products);
+        }
     }
 }
